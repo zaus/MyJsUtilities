61d1834452e8a60dcbd6e705645037a796c6e2eb
Jeremy Schwartz <jeremys.atwork@gmail.com>
2013-02-20 15:18:12 -0500

    jquery plugins - filtered trigger
    file with jquery plugins -- mainly `triggerAny` and supporting functions

---
+++ jquery-extras.js
@@ -0,0 +1,65 @@
+(function($){
+
+	/** turn event type + namespace into string */
+	var eTaN = function(e) {
+		return e.type + ( e.namespace ? '.' + e.namespace : '' );
+	};
+  
+	/** shortcut to getting the list of bound events for an element, since it may change */
+	$.fn.listEvents = function(isSorted) {
+		var result = [], /*jQuery event listing*/evts, /*list event names*/evns;
+		this.each(function() {
+			// from http://jebaird.com/blog/trigger-all-events-element-using-jquery
+			// only works for < jQuery 1.4?
+			// ### return $el.data('events') || [];
+			
+			// investigate DOM properties prefixed with "on"? http://stackoverflow.com/a/5848702/1037948 -- no, doesn't work
+			
+			// jQuery 1.8+ http://stackoverflow.com/a/571087/1037948
+			// but dangerous?: http://stackoverflow.com/a/13018025/1037948
+			evts = $._data(this, "events"); // the iffy bit, in case jQuery changes again
+			
+			if( ! evts ) return;
+			// extract type+namespace, turn into list of "readable" names
+			// remove duplicates with $.unique
+			evns = $.unique( 
+				// turn list of "event: specifics" into flat array
+				$.map(evts, function(f,n) {
+					return $.map(f, function(e) { return eTaN(e); });
+				}) //-- .map evts
+			); //-- .unique
+			// optionally sort so it looks better
+			result.push.apply(result, (isSorted ? evns.sort() : evns) ); // $.merge?
+		});//-- this.each
+		
+		console.log('>> listing events', result);
+		return result;
+	};//--	$.fn.listEvents
+
+	/** filterable trigger */
+	$.fn.triggerAny = function(filter, data){
+		// note the use of .$each vs. .each
+		return this.$each(function(){
+			var events = $.grep( this.listEvents(), function(en) {
+					// TODO: regex to "translate" wildcards
+					return -1 != (en).indexOf(filter);
+				})
+			;
+			$.each(events, function(i,en){ this.trigger( en, data ); });
+		});
+	};//--	$.fn.triggerAny
+	
+	/** like jQuery.each, but `this` in scope is the jQuery element instead */
+	$.fn.$each = function (callback) {
+		/// <summary>
+		/// Autobind each element as jQuery object to $this
+		/// </summary>
+		/// <param name="callback">loop through jQuery elements; note that `this` in scope is the jQuery object, not the DOM element</param>
+
+		return this.each(function (i, o) {
+			var $o = $(o);
+			return callback.call($o, i, $o);
+		});
+	};
+
+})(jQuery);
--- readme.md
+++ readme.md
@@ -3,7 +3,8 @@ MyJsUtilities
 
 Some useful javascript functions and stuff, cobbled together from various sources.
 
-[Testing/Demo Fiddle](http://jsfiddle.net/drzaus/cTy3Q/) _(also included in `/examples`)_
+- [Testing/Demo Fiddle](http://jsfiddle.net/drzaus/cTy3Q/) _(also included in `/examples`)_
+- [Another testing demo](http://codepen.io/zaus/pen/Bvaow) or [here](http://jsfiddle.net/drzaus/8bvar/7/)
 
 Contents
 --------
@@ -11,7 +12,11 @@ Contents
 * **Logging**: Safe `console.log` wrapper with debugmode toggling.  Inspired by [Paul Irish](http://paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/).
 * **Inheritance**: Object-oriented "macros" for class _definition_ (`Define`) and _inheritance_ (`Inherits`).  Inspired by [this](http://phrogz.net/js/classes/OOPinJS2.html) and [that](http://www.htmlgoodies.com/html5/tutorials/create-an-object-oriented-javascript-class-constructor.html)
 * **Templating**: simple [Moustache-style](https://github.com/janl/mustache.js) template helper.
-
+* **jQuery Plugins**:
+   - **listEvents**: get all bound events (jQ 1.8+)
+   - **triggerAny**: filter namespaced triggers, like `.custom1` to fire `click.custom1` and `change.custom1`
+   - **$each**: "fixes" `.each` so that `this` (and param) are jQuery, not DOM elements
+   
 
 License
 -------
